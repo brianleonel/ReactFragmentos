520e2596b7ed8c1c10498310c9a77e10f995c8a9
brianleonel <112188352+brianleonel@users.noreply.github.com>
2024-08-06 16:10:11 -0300

    Add files via upload

---
+++ Card.tsx
@@ -0,0 +1,15 @@
+
+function Card(){
+  return (<><div>
+    <h1>brianlb</h1>
+    <CardBody></CardBody>
+  </div></>);
+}
+export function CardBody(){
+  return (
+    <><p>parrafo1</p>
+    <div>
+        <h2>titulo</h2>
+    </div></>);
+}
+export default Card;
